e50d003461d3ae89d28276bbc660935b1d5d7265
igorcb <igor.batista@gmail.com>
2016-01-22 15:57:12 -0300

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Segmentation - Challenge Dev"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Segmentation - Challenge Dev"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Segmentation - Challenge Dev"
+  end
 end
