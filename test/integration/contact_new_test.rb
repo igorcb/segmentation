332372943e4d75b5a818294cee183c24d66e8be3
igorcb <igor.batista@gmail.com>
2016-01-25 18:37:16 -0300

    test contact green

--- test/integration/contact_new_test.rb
+++ test/integration/contact_new_test.rb
@@ -4,8 +4,16 @@ class ContactNewTest < ActionDispatch::IntegrationTest
 	test "invalid contact new information" do
 		get new_contact_path
 		assert_no_difference 'Contact.count' do
-			post contacts_path, contact: {name:'Igor Batista', email: "admin@admin.org", age: 30, state: 'Ceara', office: 1}
+			post contacts_path, contact: {name:'', email: "", age: 30, state: 'Ceara', office: 1}
 		end
-		assert_template 'user/new'
+		assert_template 'contacts/new'
 	end
+
+	test "valid contact new information" do
+		get new_contact_path
+    assert_difference 'Contact.count', 1 do
+      post_via_redirect contacts_path, contact: {name:'Igor Batista', email: "admin@admin.org", age: 30, state: 'Ceara', office: 1}
+    end
+    assert_template 'contacts/show'
+  end	
 end
